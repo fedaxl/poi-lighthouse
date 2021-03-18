5b27f1138631621501f6b4d8fac267f8a8727680
Federica Fiorenza <20091413@mail.wit.ie>
2021-03-18 23:46:57 +0000

    commit
    commit to main

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# poi-lighthouse
- Enterprise Web Development - Assignment 1 - web poi
+# poi-lighthouses v.1
+
+Enterprise Web Development - Assignment 1<br />
+Student 20091413<br />
+College: WIT
+
+## Description
+This application will allow a user to login/signup to an account, add and manage points of interests, add images etc. 
+The admin user will be able to view all the other users accounts and manage all the POI inserted.
+<br />
+
+### v.1.1
+Authentation/Cookies/Session (@hapi/cookie)<br />
+Images (Upload/View) (cloudinary)<br />
+SignUp/Login/Delete account <br />
+Simple User Admin Dashboard <br />
+Add POI: Name, Description, Category, Location, Image <br />
+Update/Delete POI<br />
+Organize POIs into categories <br />
+Manage POI images <br />
+
+## Technology Stack
+Name|Specification|
+|---|--------|
+|UI|handlebars, uikit|
+|Components|handlebars, mongoose, dotenv, mais-moongoose-seeder, cloudinary|
+|Framework|hapi, inert, vision, cookie, boom|
+|Platforms|node, mongodb, cloudinary|
+|Infrastructure|heroku, cloudatlas, glitch|
+<br />
+
+To try the app, you will have to install the following:
+```
+npm install @hapi/hapi
+npm install @hapi/inert
+npm install @hapi/vision
+npm install handlebars
+npm install handlebars
+npm install cloudinary
+npm install mongoose
+npm install dotenv
+```
+See layout.hbs for UIkit and Font Awesome initialisation
+
+
+## Author
+Federica Fiorenza
---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+const ImageStore = require('./app/utils/image-store');
+const Hapi = require('@hapi/hapi');
+const dotenv = require('dotenv');
+const Joi = require("@hapi/joi");
+
+const result = dotenv.config();
+if (result.error) {
+ console.log(result.error.message);
+ process.exit(1);
+}
+
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+});
+
+require('./app/models/db');
+
+// Cloudinary Credentials
+const credentials = {
+  cloud_name: process.env.name,
+  api_key: process.env.key,
+  api_secret: process.env.secret
+};
+
+async function init() {
+  await server.register(require('@hapi/inert'));
+  await server.register(require('@hapi/vision'));
+  await server.register(require('@hapi/cookie'));
+
+  server.validator(require('@hapi/joi'))
+
+  server.auth.strategy('session', 'cookie', {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false
+    },
+    redirectTo: '/'
+  });
+
+  server.auth.default('session');
+
+  ImageStore.configure(credentials);
+
+  server.views({
+    engines: {
+      hbs: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './app/views',
+    layoutPath: './app/views/layouts',
+    partialsPath: './app/views/partials',
+    layout: true,
+    isCached: false
+  });
+
+  server.route(require('./routes'));
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
